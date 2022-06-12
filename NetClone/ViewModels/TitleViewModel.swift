51ac991ec91f99ab75dc8205f609a221b634c4d4
rio.s <rio.s101101@gmail.com>
2022-06-12 18:28:05 +0400

    Custom TableViewCell + TitleViewModel

---
+++ NetClone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  NetClone
+//
+//  Created by RS on 12.06.22.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
