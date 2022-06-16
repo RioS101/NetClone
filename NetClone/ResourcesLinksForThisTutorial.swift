7ae1f5fa46b952842f346e4ab9374f4691d269a3
rio.s <rio.s101101@gmail.com>
2022-06-16 23:49:58 +0400

    helper file with some links

---
+++ NetClone/ResourcesLinksForThisTutorial.swift
@@ -0,0 +1,20 @@
+//
+//  ResourcesLinksForThisTutorial.swift
+//  NetClone
+//
+//  Created by RS on 16.06.22.
+//
+
+import Foundation
+
+/*
+ TMDB
+ https://www.themoviedb.org/settings/api
+ https://developers.themoviedb.org/3/getting-started/search-and-query-for-details
+ GOOGLE
+ https://www.google.com/search?q=google+developer+console&newwindow=1&client=safari&rls=en&sxsrf=ALiCzsbGLFfaIsJfqsIB4tLjfwBpMiVGpg%3A1655403268404&ei=BHOrYryPGJ6Hxc8PtdSH8Ag&oq=google+develo&gs_lcp=Cgdnd3Mtd2l6EAMYATIECAAQQzIECAAQQzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIICAAQgAQQyQMyBQgAEIAEOgcIABBHELADOgoIABDkAhCwAxgBOg8ILhDHARDRAxDUAhAKEENKBAhBGABKBAhGGAFQ9QhY3Axg1BRoAXABeACAAckBiAHMBJIBBTAuMi4xmAEAoAEByAENwAEB2gEGCAEQARgJ&sclient=gws-wiz
+ https://console.cloud.google.com/apis/api/youtube.googleapis.com/metrics?project=netclone-353518&supportedpurview=project
+ YOUTUBE
+ https://www.google.com/search?client=safari&rls=en&q=youtube+api&ie=UTF-8&oe=UTF-8
+ https://developers.google.com/youtube/v3/docs/search/list?apix=true
+ */
